ebb78471da2b7afd3769a822e9b84b3687ace386
iswara.chaitanya <iswara@sivananda.org>
2021-06-26 12:44:41 -0400

    change name to sortTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import { BasicTable } from "./components/BasicTable";
+import { SortTable } from "./components/SortTable";
 
 function App() {
   return (
     <div className='App'>
-      <BasicTable></BasicTable>
+      <SortTable />
     </div>
   );
 }
