2075c4634cf4e5c5dd0b382616508bd86f8c40f5
iswara.chaitanya <iswara@sivananda.org>
2021-06-27 18:23:24 -0400

    change name to filteringTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import { BasicTable } from "./components/BasicTable";
+import { FilteringTable } from "./components/FilteringTable";
 
 function App() {
   return (
     <div className='App'>
-      <BasicTable></BasicTable>
+      <FilteringTable />
     </div>
   );
 }
