6ed6dc39e8d6ec638cf4030b55bc97a7b4b0ee1a
iswara.chaitanya <iswara@sivananda.org>
2021-06-30 13:29:06 -0400

    basic pagination

--- src/components/PaginationTable.tsx
+++ src/components/PaginationTable.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useTable } from "react-table";
+import { useTable, usePagination } from "react-table";
 import MOCK_DATA from "../MOCK_DATA.json";
 import { COLUMNS, DataStructure } from "./columns";
 import "./table.css";
@@ -12,43 +12,54 @@ export const PaginationTable = () => {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    footerGroups,
-    rows,
+    page,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    state: { pageIndex },
     prepareRow,
-  } = useTable<DataStructure>({ columns, data });
+  } = useTable<DataStructure>({ columns, data }, usePagination);
 
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => (
-                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+    <>
+      {" "}
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
               ))}
             </tr>
-          );
-        })}
-      </tbody>
-      <tfoot>
-        {footerGroups.map(footerGroup => (
-          <tr {...footerGroup.getFooterGroupProps()}>
-            {footerGroup.headers.map(column => (
-              <td {...column.getFooterProps()}>{column.render("Footer")}</td>
-            ))}
-          </tr>
-        ))}
-      </tfoot>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map(row => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => (
+                  <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>
+        <span>
+          Page <strong>{pageIndex + 1}</strong> of{" "}
+          <strong>{pageOptions.length}</strong>{" "}
+        </span>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>
+      </div>
+    </>
   );
 };
