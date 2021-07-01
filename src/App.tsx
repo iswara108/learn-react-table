462d4f1a621402cc24b4d8f56eda40e540b954ec
iswara.chaitanya <iswara@sivananda.org>
2021-07-01 17:05:59 -0400

    rename to ColumnHiding

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import { BasicTable } from "./components/BasicTable";
+import { ColumnHiding } from "./components/ColumnHiding";
 
 function App() {
   return (
     <div className='App'>
-      <BasicTable></BasicTable>
+      <ColumnHiding />
     </div>
   );
 }
