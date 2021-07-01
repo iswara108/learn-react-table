d62b130f668539bb858d36ce0c7d7bb4908e064a
iswara.chaitanya <iswara@sivananda.org>
2021-07-01 17:26:51 -0400

    hide columns

--- src/components/ColumnHiding.tsx
+++ src/components/ColumnHiding.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { useTable } from "react-table";
 import MOCK_DATA from "../MOCK_DATA.json";
+import { Checkbox } from "./Checkbox";
 import { COLUMNS, DataStructure } from "./columns";
 import "./table.css";
 
@@ -15,40 +16,57 @@ export const ColumnHiding = () => {
     footerGroups,
     rows,
     prepareRow,
+    allColumns,
+    getToggleHideAllColumnsProps,
   } = useTable<DataStructure>({ columns, data });
 
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
+    <>
+      <div>
+        <div>
+          <Checkbox {...getToggleHideAllColumnsProps()} /> Toggle All
+        </div>
+        {allColumns.map(column => (
+          <div key={column.id}>
+            <label>
+              <input type='checkbox' {...column.getToggleHiddenProps()} />{" "}
+              {column.Header}
+            </label>
+          </div>
         ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => (
-                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+      </div>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
               ))}
             </tr>
-          );
-        })}
-      </tbody>
-      <tfoot>
-        {footerGroups.map(footerGroup => (
-          <tr {...footerGroup.getFooterGroupProps()}>
-            {footerGroup.headers.map(column => (
-              <td {...column.getFooterProps()}>{column.render("Footer")}</td>
-            ))}
-          </tr>
-        ))}
-      </tfoot>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => (
+                  <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map(footerGroup => (
+            <tr {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map(column => (
+                <td {...column.getFooterProps()}>{column.render("Footer")}</td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </>
   );
 };
